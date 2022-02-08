ea0c3aa694ba7d2f05000375711d8a4b8b0f7f69
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2022-02-08 16:23:09 +0100

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,25 @@
             <h1>Nous Sommes Là pour vous</h1>
             <p>Apprenez de nouvelles compétences avec nous, </p>
             <button class="regsiter_button">
-                    Register
+                Register
             </button>
+        </div>
+    </div>
+    <div class="aboutus_section">
+        <div class="about_img">
+            <img src="{{ asset("images/pp.png") }}" alt="">
+        </div>
+        <div class="about_desc">
+            <h2>IOT-FORMATIONS</h2>
+            <p>
+                Lorem Ipsum is simply dummy text of the 
+                printing and typesetting industry. Lorem 
+                Ipsum has been the industry's standard 
+                dummy text ever since the 1500s, 
+                when an unknown printer took a galley 
+                of type and scrambled it to make a type 
+                specimen book.
+            </p>
         </div>
     </div>
 @endsection
